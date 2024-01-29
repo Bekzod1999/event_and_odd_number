9bb81e47f0f9f266d8dea38c0d19f03da0fd043c
programmer_1999 <developmentfirm1999@gmail.com>
2024-01-29 21:48:46 +0300

    h

--- number_even.py
+++ number_even.py
@@ -4,17 +4,16 @@
 
 #Print the number of even digits in the variable "var_int".
 
-# var_int = 1112
+var_int = 4875
 
-# fourthDigit = var_int // 1000 #4
-# value = var_int % 1000 #5 68
-# threeDigit = value // 100 # 5
-# value2 = value % 100 # 68
-# twoDigit = value2 // 10 # 6
-# oneDigit = value2 % 10 # 8
-# sum = 0
-# sum = fourthDigit % 2 + threeDigit % 2 + twoDigit % 2 + oneDigit % 2
-# result = 4 - sum
-# print(result)
+fourthDigit = var_int // 1000 #4
+value = var_int % 1000 #875
+threeDigit = value // 100 #8
+value2 = value % 100 # 75
+twoDigit = value2 // 10 # 7
+oneDigit = value2 % 10 # 5
+sum = 0
+sum = fourthDigit % 2 + threeDigit % 2 + twoDigit % 2 + oneDigit % 2
+result = 4 - sum
+print(result)
 
-print(145//10)
